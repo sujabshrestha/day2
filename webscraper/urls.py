7695adf7c0f1d32d420078f88ecb7c17e846d43a
sujabshrestha <ssujab9@gmail.com>
2019-12-10 17:08:48 +0545

    created time teller app

--- webscraper/urls.py
+++ webscraper/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from scraperapp.views import home
-from scraperapp.views import index
+from django.urls import path,include
+from scraperapp.views import home,timestamp,today
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home),
-    path('index/',index)
+    path('',include('scraperapp.urls')),
+    
 ]
